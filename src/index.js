8bb1333478ca26337642130ebb0a4f104db0f1a3
SamurayKnight <danlee97.06@gmail.com>
2022-10-24 02:13:36 -0500

    Añadiendo Webpack

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+/*CSS*/
+import './css/all-main.css';
+import './css/main.css';
+
+/*IMÁGENES */
+import './img/006-like.png';
+import './img/013-newsletter.png';
+import './img/cover.png';
+import './img/logo.png';
+import './img/pc-img.png';
+import './img/post-1.png';
+import './img/post-2.png';
+import './img/post-3.png';
+import './img/profile-pic.png';
+
